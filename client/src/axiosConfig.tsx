79c37186499bca7094210c1bcb19fb9cea7db770
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2025-08-27 17:01:19 +0700

    Refactor axios configuration to remove fallback baseURL

--- client/src/axiosConfig.tsx
+++ client/src/axiosConfig.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseURL = process.env.REACT_APP_SERVER_URI || "https://portainer.seateklab.vn/";
+let baseURL = process.env.REACT_APP_SERVER_URI;
 const app = axios.create({
     baseURL,
     withCredentials: true, 
